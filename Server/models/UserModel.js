7a304e0b79e1f5f30f66eafda95cc1743c2158ac
Stalwartech <Kvngstalwart@gmaill.com>
2026-01-22 23:53:17 +0100

    User Schema model done successfully

--- Server/models/UserModel.js
+++ Server/models/UserModel.js
@@ -14,5 +14,35 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true
+    },
+    skillLevel:{
+        type: String,
+        enum: ["Beginner", "Intermediate", "Advance"],
+        default: "Beginner",
+    },
+    learningGoal:{
+        type: String
+    },
+    subscriptionActve: {
+        type: Boolean,
+        default: false
     }
-})
+},
+{timestamps: true}
+)
+
+// Password hashing
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) return next();
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
+
+// Password match method
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+
+module.exports = mongoose.model('User', userSchema);
