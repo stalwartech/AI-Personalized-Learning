bd9fc943b0f75b16e37f386689e91563806ad9ab
Stalwartech <Kvngstalwart@gmaill.com>
2026-01-23 20:15:08 +0100

    Authcontroller added successful

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,3 +1,26 @@
 const User = require("../models/UserModel.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const Signup = async (req, res) => {
+    const {name, email, password, skillLevel, learningGoal} = req.body
+    // Find the email if it exist 
+    // If email exist throw an error email exist else 
+    // Hash the password that is provided 
+    // Then also generate the token for the user.
+    // Save the user details to the database
+    
+    try {
+        const findUser = await User.findOne({email})
+        
+    } catch (error) {
+        
+    }
+}
+
+const Login = async (req, res) => {
+    const {email, password} = req.body
+    // Check if email exist 
+    // Compare the password with the hashed password 
+    // if true then generate a token 
+}
--- Server/models/UserModel.js
+++ Server/models/UserModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    Email:{
+    email:{
         type: String,
         unique: true, 
         required: true
