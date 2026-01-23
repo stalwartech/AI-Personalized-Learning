e41be7c14d35aab4e09d07f0ce80f6b76f859e22
Stalwartech <Kvngstalwart@gmaill.com>
2026-01-23 22:12:20 +0100

    Signup Done

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,21 +1,40 @@
 const User = require("../models/UserModel.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const env = require("dotenv").config()
 
 const Signup = async (req, res) => {
     const {name, email, password, skillLevel, learningGoal} = req.body
     // Find the email if it exist 
+    const emailExist = await User.findOne({email})
     // If email exist throw an error email exist else 
-    // Hash the password that is provided 
-    // Then also generate the token for the user.
-    // Save the user details to the database
-    
     try {
-        const findUser = await User.findOne({email})
+        if(emailExist){
+            res.send(404).json({status: false, message: "Email already exist"})
+        }
+        // Hash the password that is provided 
+        const hashedPassword = await bcrypt.hash(password, process.env.Salt);
+
+        // Then also generate the token for the user.
+        const token = jwt.sign({name,email,subscriptionActve}, process.env.JWTSecretKey, {expiresIn: "30d"})
         
+        // Save the user details to the database
+        const saveDetails = User.create({name, email, "password": hashedPassword, skillLevel, learningGoal});
+
+        if(saveDetails){
+            res.send(200).json({
+                "name": name,
+                "email": email,
+                "password": hashedPassword,
+                "skillLevel": skillLevel,
+                "learningGoal": learningGoal,
+                "token": token
+            })
+        }
     } catch (error) {
         
     }
+    
 }
 
 const Login = async (req, res) => {
