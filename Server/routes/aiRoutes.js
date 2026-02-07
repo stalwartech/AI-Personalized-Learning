3703acc6390abc43ed9a0dac9539b1f06e76936b
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-07 23:43:46 +0100

    nothing muvh

--- Server/routes/aiRoutes.js
+++ Server/routes/aiRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {generateTopicContent} = require("../controllers/aiController.js");
 const aiLimit = require("../middleware/aiLimitMiddleware.js")
-const {protect} = require("../middleware/authMiddleware.js") // autheticate and proptector 
+const {protect} = require("../middleware/authMiddleware.js") 
 
 
 router.post("/generate-topic",protect, aiLimit, generateTopicContent)
--- Server/routes/paymentRoutes.js
+++ Server/routes/paymentRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { initializePayment } = require("../controllers/paymentController");
 const {protect} = require("../middleware/authMiddleware");
 
+// No payment to be added for now 
 router.post("/pay", protect, initializePayment);
 
 module.exports = router;
