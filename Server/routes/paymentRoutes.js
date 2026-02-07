ad0cefa395b2ab79297639cac576a994ce672087
Stalwartech <abdulsalamakintaro@gmail.com>
2026-02-07 23:44:29 +0100

    nothing muvh

--- Server/routes/paymentRoutes.js
+++ Server/routes/paymentRoutes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { initializePayment } = require("../controllers/paymentController");
 const {protect} = require("../middleware/authMiddleware");
 
-// No payment to be added for now 
 router.post("/pay", protect, initializePayment);
 
 module.exports = router;
